d7d45abe987924b55260b46cb47e3879008d51b3
amyzliao <amzhliao@gmail.com>
2024-03-18 20:07:58 -0500

    more package stuff

--- node-tutorial/app.js
+++ node-tutorial/app.js
@@ -19,3 +19,6 @@ const _ = require('lodash')
 const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems)
+console.log('hello people')
+
+// info on package.json: https://nodesource.com/blog/the-basics-of-package-json/
